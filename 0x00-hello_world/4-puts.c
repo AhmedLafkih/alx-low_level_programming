c9a33b40a07a13f7f8c8a21e009ab4fc6e91998f
AhmedLafkih <ahmedlafkih@gmail.com>
2023-12-14 16:31:58 -0800

    hllo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main-the entry point
+*
+*Description : simple c programme print using puts func
+*
+*Return : 0 false
+*/
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0); }
