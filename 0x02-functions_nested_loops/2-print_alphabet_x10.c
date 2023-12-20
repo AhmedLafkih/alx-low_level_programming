795772ecf01f01291616ab8319199033812ce121
AhmedLafkih <ahmedlafkih@gmail.com>
2023-12-19 16:27:16 -0800

    kjkjk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@
 void print_alphabet_x10(void)
 {
 int ch;
-int i = 0;
-while (i < 10)
+int i;
+ for (i=0; i < 10; i++)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {_putchar(ch); }
 putchar('\n');
-++i; }
+}
 }
