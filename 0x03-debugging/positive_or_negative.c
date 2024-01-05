00782699b8fcfd1b920a4e776c1ddfa436ffbd26
AhmedLafkih <ahmedlafkih@gmail.com>
2024-01-05 07:53:50 -0800

    hard pointer

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* postitive_or_negative - function that prints if integer is positive or negative
+* @n :check the value pos or neg
+* Return: 0
+*/
+
+int postitive_or_negative(int n)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings,
+ * @dest: ptr destination input
+ * @src: ptr source input
+ * Return: the pointer to dest
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int ch1, ch2;
+
+	ch1 = 0;
+
+	while (dest[ch1])
+		ch1++;
+
+	for (ch2 = 0; src[ch2] ; ch2++)
+		dest[ch1] = src[ch2];
+
+	return (dest);
+}
